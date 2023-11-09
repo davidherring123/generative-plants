b8212d39a0997149f8fc2aea6ca4677301971f15
unknown <david.herring8680@gmail.com>
2023-11-09 02:14:53 -0600

    added base vite project

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='text-center text-3xl'>
+        Hello!
+      </div>
+    </>
+  )
+}
+
+export default App
